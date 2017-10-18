379ed329a2ff1715d75c64f81e2b9dd36cbe9380
Fabio Araujo <fabio_araujo11@hotmail.com>
2017-10-18 00:12:36 -0200

    Implementado meia parte do cadastro de usuario

--- src/java/br/com/poo/quiz/BancoUsuarios.java
+++ src/java/br/com/poo/quiz/BancoUsuarios.java
@@ -17,39 +17,40 @@ public class BancoUsuarios {
     public static ArrayList<Usuario> usuarios;
 
     public static ArrayList<Usuario> getUsuarios() {
-        if(usuarios == null){
-            usuarios = new ArrayList<>();
-            usuarios = setUsuarios();
-            return usuarios;
-        }
+//        if(usuarios == null){
+//            usuarios = new ArrayList<>();
+//            return usuarios;
+//        }
         return usuarios;
     }
 
-    public static ArrayList<Usuario> setUsuarios() {
-        Usuario vitor = new Usuario(
-        "Vitor",
-                "123",
-                0
-        );
-        usuarios.add(vitor);
-        Usuario Renan = new Usuario(
-        "Renan",
-                "123",
-                0
-        );
-        usuarios.add(Renan);
-        Usuario Victor = new Usuario(
-        "Victor",
-                "123",
-                0
-        );
-        usuarios.add(Victor);
-        Usuario Fabin = new Usuario(
-        "Fabin",
-                "123",
-                0
-        );
-        usuarios.add(Fabin);
+    public static ArrayList<Usuario> setUsuarios(String nmUsuario, String nmSenhaUsuario) {
+//        Usuario vitor = new Usuario(
+//        "Vitor",
+//                "123",
+//                0
+//        );
+//        usuarios.add(vitor);
+//        Usuario Renan = new Usuario(
+//        "Renan",
+//                "123",
+//                0
+//        );
+//        usuarios.add(Renan);
+//        Usuario Victor = new Usuario(
+//        "Victor",
+//                "123",
+//                0
+//        );
+//        usuarios.add(Victor);
+//        Usuario Fabin = new Usuario(
+//        "Fabin",
+//                "123",
+//                0
+//        );
+//        usuarios.add(Fabin);
+        Usuario newUser = new Usuario(nmUsuario,nmSenhaUsuario,0);
+        usuarios.add(newUser);
         return usuarios;
     }
     
